a88a5084636eafe507be55a3bd9112519969e687
mohammad2016mr <mohammad.kingme@gmail.com>
2016-11-14 07:11:56 -0800

    Create dati.php

---
+++ dati.php
@@ -0,0 +1,12 @@
+<?php
+
+//APIKEY
+$apiKey = "278007787:AAEfcg_h8SwK6VAYGSOH1MBYFVc1XXx5-_A";
+//TUOID
+$inserisciIlTuoId = "172561830,90516804";
+//puoi trovare il tuo id mandando un messaggio
+//a @userinfobot
+
+//BOT CREATO DA @xMicky24YT PER QUALSIASI PROBLEMA NON ESITARE A CONTATTARMI
+
+?>
